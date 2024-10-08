68689452aad25f6a62c18ceb3e80c7c3a3fa5016
bostonbrad <nissenbaumbrad@gmail.com>
2024-10-07 23:49:15 -0400

    Added routes for assigning task to clients and viewing tasks

---
+++ backend/models/Task.js
@@ -0,0 +1,12 @@
+// This model defines the structure of tasks
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    title: { type: String, required: true},
+    description: { type: String},
+    type: { type: String, enum: ['file', 'form'], required: true},
+    status: { type: String, enum['pending', 'completed'], default: 'pending'},
+    assigned_to: { type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+});
+
+module.exports = mongoose.model('Task', TaskSchema);

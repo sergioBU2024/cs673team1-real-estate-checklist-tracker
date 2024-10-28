b9542a04ecf1602f109db85d5421a37f894e5d05
sergiok10 <khalil.sergio@hotmail.com>
2024-10-28 15:43:47 -0400

    Added backend apis to frontend as callable functions.

---
+++ frontend/src/controllers/leaseApplicationsController.js
@@ -0,0 +1,88 @@
+/**********************************************Get All Applications of a specific user  *********************************************/
+const getApplications = async () => {
+    const res = await fetch('/api/applications/', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        }
+        
+    });
+
+    const data = await res.json();
+
+    if(!res.ok){
+        throw Error(data.error);
+    }
+
+    return data;
+}
+
+/**********************************************Create New Application *******************************************/
+const addApplication = async (location, userEmails) => {
+    if(!location || !userEmails){
+        throw Error('All fields are required');
+    }
+
+    const res = await fetch('/api/applications/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({location, userEmails})
+    });
+
+    const data = await res.json();
+
+    if(!res.ok){
+        throw Error(data.error);
+    }
+
+    return data;
+}
+
+/**********************************************Delete Application *******************************************/
+const deleteApplication = async (_id) => {
+    const res = await fetch(`/api/applications/${_id}`, {
+        method: 'DELETE',
+        headers: {
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        }
+    });
+
+    const data = await res.json();
+
+    if(!res.ok){
+        throw Error(data.error);
+    }
+
+    return data;
+}
+
+/**********************************************Update Application *******************************************/
+const updateApplication = async (application, location, userEmails) => {
+    if(!location || !userEmails){
+        throw Error('All fields are required');
+    }
+
+    const res = await fetch(`/api/applications/${application._id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({location, userEmails})
+    });
+
+    const data = await res.json();
+
+    if(!res.ok){
+        throw Error(data.error);
+    }
+
+    return data;
+}
+
+export { getApplications, addApplication, deleteApplication, updateApplication };
+
---
+++ frontend/src/controllers/usersController.js
@@ -0,0 +1,50 @@
+/*******************************Login User **************************************/
+const loginUser = async (email, password) => {
+    if(!email || !password){
+        throw Error('All fields are required');
+    }
+
+    const res = await fetch('/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email, password})
+    });
+
+    const data = await res.json();
+
+    if(!res.ok){
+        throw Error(data.error);
+    }
+
+    console.log(data);
+}
+
+export { loginUser };
+
+
+/*******************************Register User **************************************/
+const registerUser = async (email, password, role) => {
+    if(!email || !password || !role){
+        throw Error('All fields are required');
+    }
+
+    const res = await fetch('/api/users/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({email, password, role})
+    });
+
+    const data = await res.json();
+
+    if(!res.ok){
+        throw Error(data.error);
+    }
+
+    console.log(data);
+}
+
+export { registerUser };

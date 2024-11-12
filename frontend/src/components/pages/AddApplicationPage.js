c88f92d51746073df7d37ebdb1e07a023b329ec4
Shivaang <shivaang@bu.edu>
2024-11-12 15:00:44 -0500

    Added ability to add multiple clients to an aplpication

--- frontend/src/components/pages/AddApplicationPage.js
+++ frontend/src/components/pages/AddApplicationPage.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../Header';
-import { Box, Typography, Container, Paper, TextField, Button } from '@mui/material';
+import { Box, Typography, Container, Paper, TextField, Button, MenuItem, Grid } from '@mui/material';
 
 const AddApplicationPage = () => {
   const navigate = useNavigate();
+  const [address, setAddress] = useState({
+    street: '',
+    city: '',
+    state: '',
+    zip: ''
+  });
+  const [numApplicants, setNumApplicants] = useState(1);
+  const [applicants, setApplicants] = useState([{ firstName: '', lastName: '', email: '' }]);
+
+  const handleAddressChange = (field, value) => {
+    setAddress((prev) => ({
+      ...prev,
+      [field]: value
+    }));
+  };
+
+  const handleNumApplicantsChange = (e) => {
+    const number = parseInt(e.target.value);
+    setNumApplicants(number);
+
+    // Adjust the applicants array based on the selected number of applicants
+    setApplicants(Array.from({ length: number }, () => ({ firstName: '', lastName: '', email: '' })));
+  };
+
+  const handleApplicantChange = (index, field, value) => {
+    const updatedApplicants = [...applicants];
+    updatedApplicants[index][field] = value;
+    setApplicants(updatedApplicants);
+  };
+
+  const handleSaveApplication = () => {
+    const applicationData = {
+      address,
+      applicants,
+    };
+    console.log('Application Saved:', applicationData);
+    // Add the logic to save the application, e.g., send to an API
+  };
 
   const handleClose = () => {
     navigate('/dashboard');
@@ -18,55 +56,107 @@ const AddApplicationPage = () => {
           <Typography variant="h4" gutterBottom>
             Add New Application
           </Typography>
-          <TextField
-            fullWidth
-            label="Name"
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="Email"
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="Phone"
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="Office Location"
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            label="Apartment Address"
-            margin="normal"
-            variant="outlined"
-          />
-          <TextField
-            fullWidth
-            InputLabelProps={{ shrink: true }}
-            label="Move In Date"
-            margin="normal"
-            variant="outlined"
-            type="date"
-          />
 
+          {/* Address Fields */}
+          <Typography variant="h6" gutterBottom>Address</Typography>
           <TextField
             fullWidth
-            label="Monthly Rent Price"
+            label="Street Address"
             margin="normal"
             variant="outlined"
-            type="number"
+            value={address.street}
+            onChange={(e) => handleAddressChange('street', e.target.value)}
           />
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                fullWidth
+                label="City"
+                margin="normal"
+                variant="outlined"
+                value={address.city}
+                onChange={(e) => handleAddressChange('city', e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                fullWidth
+                label="State"
+                margin="normal"
+                variant="outlined"
+                value={address.state}
+                onChange={(e) => handleAddressChange('state', e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={3}>
+              <TextField
+                fullWidth
+                label="ZIP Code"
+                margin="normal"
+                variant="outlined"
+                value={address.zip}
+                onChange={(e) => handleAddressChange('zip', e.target.value)}
+              />
+            </Grid>
+          </Grid>
+
+          {/* Number of Applicants Dropdown */}
+          <TextField
+            fullWidth
+            label="Number of Applicants"
+            margin="normal"
+            variant="outlined"
+            select
+            value={numApplicants}
+            onChange={handleNumApplicantsChange}
+          >
+            {[1, 2, 3, 4, 5].map((num) => (
+              <MenuItem key={num} value={num}>
+                {num}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          {/* Dynamic Applicant Fields */}
+          {applicants.map((applicant, index) => (
+            <Grid container spacing={2} key={index} sx={{ mt: 2 }}>
+              <Grid item xs={12}>
+                <Typography variant="h6">Applicant {index + 1}</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  label="First Name"
+                  variant="outlined"
+                  value={applicant.firstName}
+                  onChange={(e) => handleApplicantChange(index, 'firstName', e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  fullWidth
+                  label="Last Name"
+                  variant="outlined"
+                  value={applicant.lastName}
+                  onChange={(e) => handleApplicantChange(index, 'lastName', e.target.value)}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  fullWidth
+                  label="Email"
+                  variant="outlined"
+                  value={applicant.email}
+                  onChange={(e) => handleApplicantChange(index, 'email', e.target.value)}
+                />
+              </Grid>
+            </Grid>
+          ))}
+
+          {/* Save and Cancel Buttons */}
           <Box sx={{ mt: 4, display: 'flex', gap: 2 }}>
-            <Button variant="contained" color="primary">
-              Save Application
+            <Button variant="contained" color="primary" onClick={handleSaveApplication}>
+              Add & Invite Applicants
             </Button>
             <Button variant="outlined" color="secondary" onClick={handleClose}>
               Cancel

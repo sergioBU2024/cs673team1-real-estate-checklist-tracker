911f5dcef27ddfeee616c6c4a82610fcdcddce46
sergiok10 <khalil.sergio@hotmail.com>
2024-12-09 20:29:16 -0500

    Wrap App component with BrowserRouter for routing functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom'; // Removed BrowserRouter alias
 import Login from './components/auth/Login';
-import SignUp from './components/auth/SignUp'; // Import SignUp component
+import SignUp from './components/auth/SignUp';
 import ClientHomePage from './components/pages/ClientHomePage';
-import AddApplicationPage from './components/pages/AddApplicationPage';// Add applicationpage 
+import AddApplicationPage from './components/pages/AddApplicationPage';
 import ApplicationDetailPage from './components/pages/ApplicationDetailPage';
 import AgentHomePage from './components/pages/AgentHomePage';
-import SettingsPage from './components/pages/SettingsPage';// setting page
-import SignUpClientInvitation from './components/auth/SignUpClientInvitation'; // Import SignUpClientInvitation component
+import SettingsPage from './components/pages/SettingsPage';
+import SignUpClientInvitation from './components/auth/SignUpClientInvitation';
 import UserProvider from './contexts/UserContext';
 import { ApplicationsProvider } from './contexts/ApplicationsContext';
 import UserDetailPage from './components/pages/UserDetailPage';
@@ -15,26 +15,21 @@ import TaskDetailsPage from './components/pages/TaskDetailsPage';
 
 function App() {
   return (
-
     <UserProvider>
-    <ApplicationsProvider>
-      <Router>
+      <ApplicationsProvider>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} /> {/* Sign Up route */}
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/client" element={<ClientHomePage />} />
           <Route path="/dashboard" element={<AgentHomePage />} />
           <Route path="/add-application" element={<AddApplicationPage />} />
-          <Route path="/applications/:applicationId" element={<ApplicationDetailPage />} /> 
-          <Route path="/settings" element={<SettingsPage />} /> {/* Settings route */}
+          <Route path="/applications/:applicationId" element={<ApplicationDetailPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
           <Route path="/signupnew/:userId" element={<SignUpClientInvitation />} />
           <Route path="/applications/:applicationId/users/:userId" element={<UserDetailPage />} />
           <Route path="/applications/:applicationId/users/:userId/task/:taskId" element={<TaskDetailsPage />} />
-          
         </Routes>
-      </Router>
-    </ApplicationsProvider>
-      
+      </ApplicationsProvider>
     </UserProvider>
   );
 }
